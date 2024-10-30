d965954ddc1f5dc3f06a5d259bfa2a6ea7153ca1
Osmanis <osmanis@linkapital.com.br>
2024-10-29 22:44:17 -0300

    Add Maven wrapper and Spring Boot project setup
    This commit introduces the Maven wrapper scripts for Unix and Windows, enabling consistent builds regardless of the Maven version installed locally. Additionally, it sets up a basic Spring Boot project structure with essential dependencies.

---
+++ src/main/java/com/demo/cleanarquitecture/CleanarquitectureApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cleanarquitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarquitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanarquitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/cleanarquitecture/CleanarquitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cleanarquitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarquitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
